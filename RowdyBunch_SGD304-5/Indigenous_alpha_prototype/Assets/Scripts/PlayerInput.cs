231df89031515dd20f2b34594caf8d9768cde511
DESKTOP-03IQ78V\Jake <jjb038@student.usc.edu>
2018-04-06 15:36:50 +1000

    Created basic scene with left and right movement, and jump. But right now the jump is more of a telelport need to fix that.

---
+++ RowdyBunch_SGD304-5/Indigenous_alpha_prototype/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float thrust;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+ 
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb2d.velocity = movement * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jump = new Vector2(0.0f, thrust);
+            rb2d.velocity = jump * speed;
+        }
+    }
+}
